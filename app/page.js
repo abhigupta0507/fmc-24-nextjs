b0f5aff73f08fa66295fe36a05635c6134af6aa5
menacingsoul <adarsh14103@gmail.com>
2024-08-14 19:26:52 +0530

    added clash display font

--- app/page.js
+++ app/page.js
@@ -4,10 +4,12 @@ import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
 import Guests from "./components/Guests";
 import Sponsors from "./components/Sponsors";
+
+
 export default function Home() {
   return (
     <>
-    <div className="text-2xl mx-auto text-center ">
+    <div className="text-2xl mx-auto text-center" style={{ fontFamily: "'Clash Display', sans-serif" }}>
     FMC WEEKEND '24
    </div>
     <NavBar />
