28c53e7bf5a0007bbaf807f82331be7979bb9548
Nishant Rihan <159631600+mainlyriihann@users.noreply.github.com>
2024-08-22 13:07:15 +0530

    Update Events.jsx

--- app/components/Events.jsx
+++ app/components/Events.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function Events(){
   return (
-    <div className='text-white'>Events</div>
+     <div>
+
+      <table>
+        <tr>
+       <img></img>
+       <img></img>
+       <img></img>
+        <img></img>
+        <img></img>
+        </tr>
+        <tr>
+          <td>Photography</td>
+          <td>Cinematography</td>
+          <td>Animation</td>
+          <td>Design</td>
+          <td>Outreach</td>
+          <td>media</td>
+           </tr>
+      </table>
+    </div>
   )
 }
