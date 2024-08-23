c186dfdd7e535bed24fab2d548451ee321778180
the-dg04 <devgup04@gmail.com>
2024-08-24 02:30:40 +0530

    fixed sponsors

--- app/components/Sponsors.jsx
+++ app/components/Sponsors.jsx
@@ -4,7 +4,6 @@ import group from "../../public/Group 176.png";
 
 const Sponsors = () => {
   return (
-    <>
         <div className="flex flex-wrap justify-center items-center">
          <div className="z-10 flex items-center justify-center w-full mt-20">   
            <div className="font-clash text-white text-5xl lg:text-7xl font-bold leading-[127.92px] text-center">
@@ -19,7 +18,7 @@ const Sponsors = () => {
           alt="Sponsors"
           />
         </div>
-    </>
+    </div>
   );
 };
 
