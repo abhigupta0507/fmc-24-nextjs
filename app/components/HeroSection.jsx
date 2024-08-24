adfa9cfb30ad1b6c27cbdeed2c1815f5ea3cb0c5
the-dg04 <devgup04@gmail.com>
2024-08-24 08:12:04 +0530

    added capsules to mobile view

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -9,10 +9,10 @@ const HeroSection = () => {
       <button className="text-white bg-red-600 mt-[20px] mb-[10px] px-4 py-2 rounded-full border-none outline-none w-[194px] h-[56px] opacity-0 animate-fade animate-delay-300 animate-fill-forwards">
         Explore More
       </button>
-      <div className="w-full justify-center hidden sm:flex opacity-0 animate-fade animate-delay-700 animate-fill-forwards">
+      <div className="w-full justify-center flex opacity-0 animate-fade animate-delay-700 animate-fill-forwards">
         <Image src="./hero_capsules.svg" width={956} height={422}/>
       </div>
-      <div className="relative top-[-14vh] w-full flex justify-center opacity-0 animate-fade animate-delay-1000 animate-fill-forwards">
+      <div className="relative top-[-6vh] sm:top-[-14vh] w-full flex justify-center opacity-0 animate-fade animate-delay-1000 animate-fill-forwards">
         <Image src="./handles.svg" width={956} height={161}/>
       </div>
     </div>
