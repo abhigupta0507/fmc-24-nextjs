b3a943de953f9feac8b7bdf780b9baef3957124e
abhigupta0507 <abhishekgupta39585@gmail.com>
2024-08-30 19:17:03 +0530

    made layout for aboutus(responsive) and improved dashboard

--- app/aboutUs/page.jsx
+++ app/aboutUs/page.jsx
@@ -1,88 +1,82 @@
 import React from "react";
-
-import Photography from "../../public/Img/EventImages/photography.png";
-import Cinematography from "../../public/Img/EventImages/cinematography.png";
-import Animation from "../../public/Img/EventImages/animation.png";
-import Outreach from "../../public/Img/EventImages/outreach.png";
-import Design from "../../public/Img/EventImages/design.png";
 import sample from "../../public/Img/EventImages/sculpture.png";
 import bg from "../../public/Img/EventImages/bg.png";
 
-function MemberCard() {
+function BackgroundMaker() {
   return (
-    <div className="bg-black rounded-lg p-2 text-white flex items-center justify-between border border-white">
-    <div className="flex items-center">
-      <img src={sample.src} alt="Photoart" className="w-1/4 h-auto object-cover rounded-lg" />
-      <h3 className="text-4xl font-semibold ml-24">Photoart</h3>
-    </div>
-  </div>
+    <div
+      className="absolute inset-0"
+      style={{
+        backgroundImage: `url(${bg.src})`,
+        backgroundRepeat: "repeat",
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        backgroundColor: "black",
+      }}
+    ></div>
   );
 }
-const AboutUs = ({ cardTextSize = "text-lg" }) => {
+
+function Header({ children }) {
+  return (
+    <header className="relative py-16 text-center">
+      <h1 className="text-white text-4xl">{children}</h1>
+    </header>
+  );
+}
+
+function Teamblock({ teamName, children }) {
+  return (
+    <section className="relative py-16">
+      <h2 className="text-4xl md:text-4x1 font-bold text-center mb-16">
+        {teamName}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 px-4 md:px-72">
+        {children}
+      </div>
+    </section>
+  );
+}
+
+function MemberCard({ name = "unknown", photo = sample.src }) {
+  return (
+    <div className="bg-black rounded-lg p-4 text-white flex items-center justify-between border border-white">
+      <img
+        src={photo}
+        alt={name}
+        className="w-16 h-16 object-cover rounded-lg"
+      />
+      <div className="flex-1 flex justify-center">
+        <h3 className="text-2xl font-semibold">{name}</h3>
+      </div>
+    </div>
+  );
+}
+
+const AboutUs = () => {
   return (
     <div className="relative bg-black text-white font-sans min-h-screen">
-      <div
-        className="absolute inset-0"
-        style={{
-          backgroundImage: `url(${bg.src})`,
-          backgroundRepeat: "repeat",
-          backgroundSize: "contain",
-          backgroundPosition: "center",
-          backgroundColor: "black",
-        }}
-      ></div>
-
-      <section className="relative flex flex-col items-center justify-center h-screen text-center px-4">
-        {/* <div className="relative z-10 mb-8">
-          <h1 className="text-4xl md:text-6xl font-extrabold mb-4 leading-tight">
-            Participate in
-            <br />
-            exciting events
-          </h1>
-          <p className="text-24 md:text-28 max-w-md mx-auto">
-            in various genres such as photography, cinematography,
-            <br />
-            animation, media, design, and outreach!
-          </p>
-        </div> */}
-
-        {/* Event Cards */}
-      </section>
-
-      {/*PHOTOGRAPHY Section*/}
-      <section className="relative z-10 py-16">
-        <h2 className="text-6xl md:text-4x1 font-bold text-center mb-16">
-          PHOTOGRAPHY
-        </h2>
-        <div className="grid grid-cols-2 gap-8 px-4 md:px-72">
-          <MemberCard />
-          <MemberCard />
-          <MemberCard />
-          <MemberCard />
-        </div>
-      </section>
-      <section className="relative z-10 py-16">
-        <h2 className="text-6xl md:text-4x1 font-bold text-center mb-16">
-          PHOTOGRAPHY
-        </h2>
-        <div className="grid grid-cols-2 gap-8 px-4 md:px-72">
-          <MemberCard />
-          <MemberCard />
-          <MemberCard />
-          <MemberCard />
-        </div>
-      </section>
-      <section className="relative z-10 py-16">
-        <h2 className="text-6xl md:text-4x1 font-bold text-center mb-16">
-          PHOTOGRAPHY
-        </h2>
-        <div className="grid grid-cols-2 gap-8 px-4 md:px-72">
-          <MemberCard />
-          <MemberCard />
-          <MemberCard />
-          <MemberCard />
-        </div>
-      </section>
+      <BackgroundMaker />
+      <Header>Meet the heroes who organised this event</Header>
+      <Teamblock teamName="Design">
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+      </Teamblock>
+      <Teamblock teamName="Technical">
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+      </Teamblock>
+      <Teamblock teamName="Marketing">
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+      </Teamblock>
     </div>
   );
 };
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,93 +1,50 @@
-// "use client";
-
-// import React, { useEffect, useState } from "react";
-// import bg from "./bg.png";
-
-// const Dashboard = () => {
-//   const [userData, setUserData] = useState(null);
-
-//   useEffect(() => {
-//     setUserData({
-//       name: "John Doe",
-//       email: "john.doe@example.com",
-//       mobile: "+1234567890",
-//       age: "18",
-//       events: [
-//         { id: 1, type: "Design",name:"Event of design" },
-//         { id: 2, type: "Cinematography",name:"Event of cinematography" },
-//         { id: 3, type: "Photography",name:"Event of photography" },
-//         { id: 4, type: "Animation",name:"Event of animation" },
-//         { id: 5, type: "Outreach",name:"Event of outreach" },
-//       ],
-//     });
-//   }, []);
-
-//   return (
-//     <div className="relative bg-black text-white font-sans min-h-screen">
-//       <div
-//         className="absolute inset-0"
-//         style={{
-//           backgroundImage: `url(${bg.src})`,
-//           backgroundRepeat: "repeat",
-//           backgroundSize: "contain",
-//           backgroundPosition: "center",
-//           backgroundColor: "black",
-//         }}
-//       ></div>
-
-//       <section
-//         className="relative flex flex-col items-center justify-center min-h-screen text-center px-4 pt-16"
-//         style={{ paddingTop: "5rem" }}
-//       >
-//         {userData ? (
-//           <>
-//             <div className="relative mb-8">
-//               <h1 className="text-4xl md:text-6xl font-extrabold mb-4 leading-tight">
-//                 Welcome, {userData.name}
-//               </h1>
-//               <p className="text-lg md:text-xl max-w-md mx-auto mb-4">
-//                 Email: <span className="font-semibold">{userData.email}</span>
-//               </p>
-//               <p className="text-lg md:text-xl max-w-md mx-auto mb-4">
-//                 Age: <span className="font-semibold">{userData.age}</span>
-//               </p>
-//               <p className="text-lg md:text-xl max-w-md mx-auto mb-8">
-//                 Mobile: <span className="font-semibold">{userData.mobile}</span>
-//               </p>
-//               <p className="text-lg md:text-xl max-w-md mx-auto">
-//                 Here are the events you are registered for:
-//               </p>
-//             </div>
-
-//             <div className="">
-//               {userData.events.map((event) => (
-//                 <div
-//                   key={event.id}
-//                   className="mt-2 bg-black rounded-lg p-4 text-white flex flex-col items-center justify-center border border-white"
-//                 >
-//                   <h3 className="text-4xl font-semibold text-center">
-//                     {event.type}
-//                   </h3>
-//                   <p className="text-4xl font-semibold text-center">{event.name}</p>
-//                 </div>
-//               ))}
-//             </div>
-//             <section className="mb-20"></section>
-//           </>
-//         ) : (
-//           <p className="relative text-lg">Loading...</p>
-//         )}
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default Dashboard;
-
 "use client";
 
 import React, { useEffect, useState } from "react";
 import bg from "./bg.png";
+
+function BackgroundMaker() {
+  return (
+    <div
+      className="absolute inset-0"
+      style={{
+        backgroundImage: `url(${bg.src})`,
+        backgroundRepeat: "repeat",
+        backgroundSize: "contain",
+        backgroundPosition: "center",
+        backgroundColor: "black",
+      }}
+    ></div>
+  );
+}
+
+function DisplayInfo({ dataType, value }) {
+  return (
+    <p className="text-lg md:text-xl max-w-md mx-auto mb-8">
+      {dataType} <span className="font-semibold">{value}</span>
+    </p>
+  );
+}
+
+function EventCardOfUser({ eventCard }) {
+  return (
+    <li
+      key={eventCard.id}
+      className="mt-2 bg-black rounded-lg p-4 text-white flex flex-col items-center justify-center border border-white"
+    >
+      <h3 className="text-2xl md:text-3xl font-semibold text-center">
+        {eventCard.type}
+      </h3>
+      <p className="text-lg md:text-xl font-normal text-center mt-2">
+        {eventCard.name}
+      </p>
+    </li>
+  );
+}
+
+function Loader() {
+  return <p className="relative text-lg">Loading...</p>;
+}
 
 const Dashboard = () => {
   const [userData, setUserData] = useState(null);
@@ -110,17 +67,7 @@ const Dashboard = () => {
 
   return (
     <div className="relative bg-black text-white font-sans min-h-screen">
-      <div
-        className="absolute inset-0"
-        style={{
-          backgroundImage: `url(${bg.src})`,
-          backgroundRepeat: "repeat",
-          backgroundSize: "contain",
-          backgroundPosition: "center",
-          backgroundColor: "black",
-        }}
-      ></div>
-
+      <BackgroundMaker />
       <section
         className="relative flex flex-col items-center justify-center min-h-screen text-center px-4 pt-16"
         style={{ paddingTop: "5rem" }}
@@ -131,39 +78,23 @@ const Dashboard = () => {
               <h1 className="text-4xl md:text-6xl font-extrabold mb-4 leading-tight">
                 Welcome, {userData.name}
               </h1>
-              <p className="text-lg md:text-xl max-w-md mx-auto mb-4">
-                Email: <span className="font-semibold">{userData.email}</span>
-              </p>
-              <p className="text-lg md:text-xl max-w-md mx-auto mb-4">
-                Age: <span className="font-semibold">{userData.age}</span>
-              </p>
-              <p className="text-lg md:text-xl max-w-md mx-auto mb-8">
-                Mobile: <span className="font-semibold">{userData.mobile}</span>
-              </p>
+              <DisplayInfo dataType="Mobile Number" value={userData.mobile} />
+              <DisplayInfo dataType="Age" value={userData.age} />
+
               <p className="text-lg md:text-xl max-w-md mx-auto">
                 Here are the events you are registered for:
               </p>
             </div>
 
-            <div className="">
+            <ul>
               {userData.events.map((event) => (
-                <div
-                  key={event.id}
-                  className="mt-2 bg-black rounded-lg p-4 text-white flex flex-col items-center justify-center border border-white"
-                >
-                  <h3 className="text-2xl md:text-3xl font-semibold text-center">
-                    {event.type}
-                  </h3>
-                  <p className="text-lg md:text-xl font-normal text-center mt-2">
-                    {event.name}
-                  </p>
-                </div>
+                <EventCardOfUser eventCard={event} />
               ))}
-            </div>
+            </ul>
             <section className="mb-20"></section>
           </>
         ) : (
-          <p className="relative text-lg">Loading...</p>
+          <Loader />
         )}
       </section>
     </div>
@@ -171,4 +102,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
