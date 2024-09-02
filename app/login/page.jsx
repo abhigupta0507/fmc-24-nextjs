0808ca38a0e2cb8e50acca8cb49b13559824f998
the-dg04 <devgup04@gmail.com>
2024-09-02 17:53:45 +0530

    efesdsdsa

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -21,7 +21,7 @@ function Login() {
   // console.log(clientid);
 
   const handleLogin=async(user)=>{
-    const res=await fetch(`${process.env.BACKEND_URL}/api/auth/login`,{
+    const res=await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/login`,{
       method:"POST",
       headers: {'Content-Type': 'application/json'},
       body:JSON.stringify({email:user.email}),
