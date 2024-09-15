6e265b9c4f2678e00bde0d7c12643752adad404b
YOGESHGARG2004 <gargyogesh2004@gmail.com>
2024-09-16 00:27:20 +0530

    glass boxes are made square in shape

--- app/components/Sponsors.jsx
+++ app/components/Sponsors.jsx
@@ -38,18 +38,18 @@ const Sponsors = () => {
         
         <motion.div className="grid justify-center justify-items-center content-center items-center col-span grid-cols-4 md:grid-cols-4 gap-2 mx-3 rounded-2xl  p-5" initial="hidden" whileInView="visible" viewport={{ once: false, amount: 0.5 }}> 
           <div className="col-span-4 md:col-span-6 backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl"><Image src={group} width={700} height={100} alt="Sponsors"/></div>
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser1} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser2} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser3} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser4} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser5} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser6} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser7} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser8} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser9} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser10} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser11} width={130} height={50} alt="Sponsors" />
-          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 rounded-2xl" src={sponser12} width={130} height={50} alt="Sponsors" />
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser1} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }} />
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser2} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }} />
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser3} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }} />
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser4} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }} />
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser5} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }} />
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser6} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }} />
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser7} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }} />
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser8} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }} />
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser9} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }}/>
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser10} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }} />
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser11} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }}/>
+          <Image className="backdrop-blur-md bg-gray-400/10 relative overflow-hidden p-2 w-[130px] h-[130px] rounded-2xl" src={sponser12} width={130} height={50} alt="Sponsors" style={{ objectFit: "contain" }} />
         </motion.div>
     </motion.div>
     </div>
